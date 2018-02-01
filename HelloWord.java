818d036a3b05d70b841e632e34bb5daefee0755a
Aki <aki353@hotmail.com>
2018-02-01 22:16:01 +0200

    more testing

--- HelloWord.java
+++ HelloWord.java
@@ -2,5 +2,7 @@
 public class HelloWord {
     public static void main(String[] args) {
         System.out.println("Hello Word!!");
+        System.out.println("(and typo's)\n");
+        System.out.println("Testing branch");
     }
 }
